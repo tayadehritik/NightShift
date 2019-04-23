68046847179df89d3b654fd2442bd3dd598723f5
braceritchie <braceritchie@protonmail.com>
2019-04-24 00:17:24 +0530

    deleted files from repo :

--- Tester.java
+++
@@ -1,9 +0,0 @@
-import Pacman.PackageClass;
-
-public class Tester
-{
-    public static void main(String args[])
-    {
-        PackageClass a = new PackageClass();
-    }
-}
--- newClass.java
+++
@@ -1,8 +0,0 @@
-import java.applet.*;
-import java.awt.*;
-
-public class newClass extends Applet {
-   public void paint (Graphics g) {
-      g.drawString("Tutorialspoint.com", 300, 150);
-   }
-}
