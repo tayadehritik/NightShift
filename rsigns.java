5d411beda5d5ba9e956a349d0dca34bb337732c6
braceritchie <braceritchie@protonmail.com>
2019-06-10 15:50:53 +0530

    rsigns started

---
+++ rsigns.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.math.*;
+
+
+class rsigns
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int l=0;l<t;l++)
+		{
+			String ak = sc.next();
+			BigInteger k = new BigInteger(ak);
+		
+		}
+	
+	}
+
+}
