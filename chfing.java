988a5b2946098e77667c25910aa03b13e433b6a1
braceritchie <braceritchie@protonmail.com>
2019-06-17 00:50:55 +0530

    no idea on how to solve chfing.java

---
+++ chfing.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.math.*;
+
+class chfing
+{
+	public static void main(String args[])
+	{
+	
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+
+		for(int l=0;l<t;l++)
+		{
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+
+			
+		
+		}
+	}
+
+}
