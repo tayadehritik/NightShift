95a65c339034d5bb32f9b7e764db286b143e636c
braceritchie <braceritchie@protonmail.com>
2019-06-24 20:25:01 +0530

    with editorial wa

--- chfing.java
+++ chfing.java
@@ -14,7 +14,33 @@ class chfing
 			int n = sc.nextInt();
 			int k = sc.nextInt();
 
+			int tastiness = k-1;
+			int i = 2;
+
+			int n1 = k;
+			int n2 = k+n-1;
+			int firstnum = n1;
+			int secondnum = n2;
+			while(true)
+			{
+				int temp = secondnum;
+				firstnum = n1 * i;
+				secondnum = n2 * i;
+
+				if(firstnum - temp <= 0)
+				{
+					break;
+				}
+				else
+				{
+					
+					tastiness = tastiness + (firstnum -temp-1);
+				}
 			
+			}
+		
+
+			System.out.println(tastiness);
 		
 		}
 	}
