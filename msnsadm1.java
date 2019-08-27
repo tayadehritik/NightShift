deb755b2b4d0c139221e1901b3eea3fae994dd88
braceritchie <braceritchie@protonmail.com>
2019-08-27 18:57:07 +0530

    august18b

---
+++ msnsadm1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class msnsadm1
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t!=0)
+		{
+
+			t--;
+		}
+	}
+}
+
