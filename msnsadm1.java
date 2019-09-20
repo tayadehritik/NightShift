cdca9be41967a6285878bdb1d2a9799df5293396
braceritchie <braceritchie@protonmail.com>
2019-09-20 22:20:22 +0530

    msnsadm1 commit one

--- msnsadm1.java
+++ msnsadm1.java
@@ -9,10 +9,34 @@ public class msnsadm1
 		while(t!=0)
 		{
 			int n = sc.nextInt();
-			int[] a = new int[n];
-			int[] b = new int[n];
+			//20 points for goal
+			//10 points for foul
+			//<0 points goes to 0 points
+			int[] a_goals = new int[n];
+			int[] b_fouls = new int[n];
+			
+			int max = a_goals[0];
 
+			for(int i=0;i<n;i++)
+			{
+				
+				int mul-goal = a_goals[i] * 20;
+				int mul-foul = b_fouls[i] * 10;
+				int final_points = mul-goal - mul-foul;
+				if(final_points<0)
+				{
+					final_points = 0;
+				}
 
+				if(final_points>max)
+				{
+					max = final_points;
+				}
+
+			}
+
+			System.out.println(max);
+			
 
 			t--;
 		}
