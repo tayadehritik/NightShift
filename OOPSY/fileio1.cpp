aa2fdc3a819312ad2b28b393f4641a4a5dbeb9ac
skinnyhrit <braceritchie@protonmail.com>
2019-12-04 21:29:49 +0530

    Program to read number of line, word and characters in a file

--- OOPSY/fileio1.cpp
+++ OOPSY/fileio1.cpp
@@ -1,13 +1,48 @@
 #include<iostream>
 #include<fstream>
-#include<string>
+#include<cstring>
 using namespace std;
 
 int main()
 {
 
     ifstream inobj;
-    char Data[80];
+    int lines;
+    int words;
+    int characters;
 
+    char Data[80];
+    inobj.open("hritik.txt");
+    if(!inobj)
+    {
+        cout<<"error";
+    }
+    else
+    {
+
+        //inobj.read((char *)&Data, sizeof(Data));
+        cout<<Data;
+
+        while(inobj)
+        {
+            inobj.getline(Data, 80);
+            int n = strlen(Data);
+            lines++;
+            characters = characters + n;
+            for(int i=0;i<n;i++)
+            {
+                if(Data[i] == ' ')
+                {
+                    words++;
+                }
+            }
+        }
+
+        cout<<endl<<"lines are "<<lines;
+        cout<<endl<<"Words are "<<words;
+        cout<<endl<<"Characters are "<<characters;
+
+        inobj.close();
+    }
     return 0;
 }
