b74fe4e958e69ed89c8e5d228efaaa148a005ba3
skinnyhrit <braceritchie@protonmail.com>
2019-12-04 21:16:06 +0530

    second program

--- OOPSY/fileio.cpp
+++ OOPSY/fileio.cpp
@@ -3,3 +3,26 @@
 #include<stdlib.h>
 
 using namespace std;
+
+int main()
+{
+    ifstream in;
+    char Data[80];
+    in.open("hritik.txt");
+    if(!in)
+    {
+        cerr<<"error";
+        exit(1);
+    }
+    else
+    {
+        cout<<endl<<"Contents of file are"<<endl;
+        while(in)
+        {
+            in.getline(Data, 80);
+            cout<<endl<<Data;
+        }
+        in.close();
+    }
+    return 0;
+}
---
+++ OOPSY/fileio1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+    ifstream inobj;
+    char Data[80];
+
+    return 0;
+}
