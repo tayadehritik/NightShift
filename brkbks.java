feaa9f0c72bd134bf02e94d0e4e6b4a1496c25ad
tayadehritik <braceritchie@protonmail.com>
2020-01-05 17:31:03 +0530

    jan long challege brkbks.java

---
+++ brkbks.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class brkbks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int strength = sc.nextInt();
+            int hit = 0;
+            int temp = 0;
+            int sum = 0;
+            int width[] = new int[3];
+            for(int j=0;j<3;j++)
+            {
+                width[j] = sc.nextInt();
+                sum = sum + weights[j];
+            }
+
+            
+            System.out.println(hits);
+
+        }
+    }
+}
