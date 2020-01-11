45432ee2013989527fd8cf861d07fbdd6c45b07d
tayadehritik <braceritchie@protonmail.com>
2020-01-11 17:33:23 +0530

    brbks completed

--- brkbks.java
+++ brkbks.java
@@ -10,27 +10,23 @@ class brkbks
         {
             int strength = sc.nextInt();
             int hit = 0;
-            int frontsum = 0;
-            int backsum = 0;
-            int fronthit = 0;
-            int backhit = 0;
-            int tempsum = 0;
-            int sum = 0;
+            
             int width[] = new int[3];
             for(int j=0;j<3;j++)
             {
                 width[j] = sc.nextInt();
-                sum = sum + width[j];
+                
             }
 
             int temp = 0;
             int trav = 0;
             int mode = 0;
-            while(trav < 3 && trav >= 0 )
+            while(trav < 3 && trav >=0)
             {
+                temp = temp + width[trav];
                 if(mode == 0)
-                {
-                    temp = temp + width[trav];
+                {   
+                    
 
                     if(temp == strength)
                     {
@@ -40,18 +36,24 @@ class brkbks
                     else if(temp > strength)
                     {
                         temp = temp - width[trav-1];
-
-                        if(temp < strength)
+                        hit = hit + 1;
+                        /*if(width[trav-1] < strength)
                         {
                             hit = hit + 1;
-                            temp = 0;
-                        }
+                            
+                        }*/
 
-                        trav = trav -1;
+                        
+
+                        //trav = trav -1;
                         
                     }
 
                     trav = trav + 1;
+                }
+                else if(mode == 1)
+                {
+                    
                 }
                 
 
@@ -61,7 +63,7 @@ class brkbks
 
             if(temp > 0 && temp <= strength)
             {
-                temp = 0;
+                
                 hit = hit + 1;
             }
             
