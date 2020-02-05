38c3604b0c095624a2df666d4e27ecd0cc991adf
tayadehritik <braceritchie@protonmail.com>
2020-02-05 19:47:27 +0530

    BinaryThreadedSearchTree implementation in cpp

---
+++ ADSL/BinaryThreadedSearchTree.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class node
+{
+    public:
+        node *right, *left;
+        int lbit, rbit;
+
+};
+
+class BTree
+{
+    public:
+    node *root;
+
+    void create(node *temp)
+    {
+
+    }
+
+    void insert(node *temp, node *trav)
+    {
+
+    }
+
+    void displayInOrder(node *root)
+    {
+
+    }
+
+    void displayPreOrder(node *root)
+    {
+
+    }
+
+    void displayPostOrder(node *root)
+    {
+        
+    }
+
+};
+
+
+int main()
+{
+    return 0;
+}
