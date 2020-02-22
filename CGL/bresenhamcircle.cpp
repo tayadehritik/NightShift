0d5c620d8f424589e5a6cc76f295757f86e4398b
tayadehritik <braceritchie@protonmail.com>
2020-02-22 11:21:12 +0530

    bug in bresenhamcircle

---
+++ CGL/bresenhamcircle.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<graphics.h>
+
+using namespace std;
+
+class BresCircle
+{
+	private:
+		int xc, yc, x, y,d,r;
+	public:
+		BresCircle()
+		{
+			cout<<endl<<"Enter center x and y";
+			cin>>xc>>yc;
+			cout<<endl<<"Enter radius for circle";
+			cin>>r;
+	
+		}
+
+		void drawCircle(int nx, int ny)
+		{
+			putpixel(xc+nx, yc+ny,RED);
+			putpixel(xc+nx, yc-ny,RED);
+			putpixel(xc-nx, yc+ny,RED);
+			putpixel(xc-nx, yc-ny,RED);
+			putpixel(xc+ny, yc+nx,RED);
+			putpixel(xc+ny, yc-ny,RED);
+			putpixel(xc-ny, yc+ny,RED);
+			putpixel(xc-ny, yc-ny,RED);
+			cout<<"here1";
+			
+
+		}
+
+		void bresCircle()
+		{
+			d = 3-(2*r);
+			for(x=xc,y=yc;y>=x;x++)
+			{
+				drawCircle(x,y);
+				cout<<d;
+				if(d<0)
+				{
+					d = d + (4*x) + 6;
+				}
+				else
+				{
+					y = y-1;
+					d = d + (4*(x-y)) + 10;
+					
+				}
+					
+			}
+
+		}
+};
+
+int main()
+{
+	int gd=DETECT,gm;
+	initgraph(&gd,&gm,NULL);
+	BresCircle c;
+	c.bresCircle();
+	
+	return 0;
+}
