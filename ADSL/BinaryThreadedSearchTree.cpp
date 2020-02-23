553170aa9dccf9a9771beffd56bee7f9d4da0f21
tayadehritik <braceritchie@protonmail.com>
2020-02-23 10:52:09 +0530

    sunday

--- ADSL/BinaryThreadedSearchTree.cpp
+++ ADSL/BinaryThreadedSearchTree.cpp
@@ -3,12 +3,55 @@ using namespace std;
 
 class node
 {
-	int data;
-	node *left,*right;
-	bool leftChild, rightChild;
+	public:
+		int data;
+		node *left,*right;
+		bool leftChild, rightChild;
 };
+
+class BinaryThreadedSearchTree
+{
+	private:
+		node *root;
+	public:
+		BinaryThreadedSearchTree()
+		{
+			root = NULL;	
+		}
+
+		void insert(int data)
+		{
+			node *temp = new node;
+			temp->data = data;
+			temp->left = NULL;
+			temp->right = NULL;
+			if(root == NULL)
+			{
+				root = temp;	
+			}
+			else
+			{
+			
+			
+			}
+		}
+		void inorderSingleThread()
+		{
+			cout<<root->data;
+		}
+
+		node* leftMost(root)
+		{
+			node *curr = root;
+
+		}
+};
+
 
 int main()
 {
+	BinaryThreadedSearchTree btree;
+	btree.insert(10);
+	btree.inorderSingleThread();
 	return 0;
 }
