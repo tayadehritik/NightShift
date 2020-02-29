cfcab8500a96e9e35b69718886f818bb00559291
tayadehritik <braceritchie@protonmail.com>
2020-02-29 15:39:50 +0530

    partiall solved dynamo

--- dynamo.java
+++ dynamo.java
@@ -1,23 +1,50 @@
 import java.util.*;
 import java.math.*;
 import java.lang.*;
+import java.io.*;
 class dynamo
 {
-    public static void main(String args[])
+    public static void main(String args[]) throws IOException
     {
         Scanner sc = new Scanner(System.in);
-       	int abc = convert(3);
-        int t = sc.nextInt();
+	int t = sc.nextInt();
+
         for(int k=0;k<t;k++)
         {
+		
             int n = sc.nextInt();
-            int a = sc.nextInt();
-            
+            long a = sc.nextLong();
+	    long retnum = convert(n);
+	    
+    	    long s = a + retnum + retnum;	    
+            System.out.println(s);
+	    System.out.flush(); 
+	    long b = sc.nextLong();
+	    long c = retnum - b;
+	    System.out.println(c);
+	    System.out.flush();
+	    long d = sc.nextLong();
+	    long e = retnum - d;
+	    System.out.println(e);
+	    System.out.flush();
+
+	   	
+	    int finaln = sc.nextInt();
+	    if(finaln == -1)
+	    {
+		System.exit(1);
+	    	break;
+	    }
+
+	    
+	
         }
+	System.out.close();
+	sc.close();
 
     }
 
-    public static int convert(int a)
+    public static long convert(int a)
     {
 	StringBuilder s = new StringBuilder("");;
 	for(int i=0;i<a;i++)
@@ -25,8 +52,7 @@ class dynamo
 		s.append("9");
 	}
 	
-	int retnum = Integer.parseInt(s.toString());
-	System.out.println(retnum);
-    	return retnum;
+	long retnum = Integer.parseInt(s.toString());
+    	return retnum+1;
     }
 }
