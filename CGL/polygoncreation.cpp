6a6ee56f9392882cff861e6e1334b19b82908b5c
tayadehritik <braceritchie@protonmail.com>
2020-03-02 15:08:41 +0530

    coords right

--- CGL/polygoncreation.cpp
+++ CGL/polygoncreation.cpp
@@ -26,17 +26,18 @@ class Line:public pixel
 		Line(int nx1, int ny1, int nx2, int ny2)
 		{
 			getdata(nx1,ny1,nx2,ny2);
-			dx = abs(x2-x1);
-			dy = abs(y2-y1);
+			dx = (x2-x1);
+			dy = (y2-y1);
 			e = (2*dy)-dx;
 			
 
 		}
 		void draw()
 		{
-			for(int x = x1,y = y1;y<=y2;x++)
+			for(int x = x1,y = y1;x<=x2;x++)
 			{
-				cout<<"here";
+				cout<<x<<"----"<<x2;
+				cout<<y<<"----"<<y2;
 				putpixel(round(x),round(y),RED);
 				if(e < 0)
 				{
@@ -48,6 +49,7 @@ class Line:public pixel
 					y = y + 1;
 				}
 				
+				
 			}		
 		}
 
@@ -57,9 +59,10 @@ int main()
 {
 	int gd=DETECT,gm;
 	initgraph(&gd,&gm,NULL);
-	Line l1(50,50,100,50);
-	
-	l1.draw();
+	//Line l1(50,50,100,50);
+	Line l2(20,20,20,40);
+	//l1.draw();
+	l2.draw();
 	getch();
 	return 0;
 }
