44e12a6dffe35cb8e612eba993d7cd5c14ba84ed
tayadehritik <braceritchie@protonmail.com>
2020-03-04 21:12:49 +0530

    structure ready

--- ADSL/Assignment1.cpp
+++ ADSL/Assignment1.cpp
@@ -1,6 +1,47 @@
 #include<iostream>
 using namespace std;
 
+class node
+{
+	private: 
+		node *left, *right;
+		int data;
+};
+
+
+class BTree
+{
+	private: 
+		node *root;
+	public:
+		void create();
+		void min();
+		void max();
+		void displayInOrder();
+		void displayPreOrder();
+		void displayPostOrder();
+
+};
+
+void BTree::create()
+{}
+
+void BTree::min()
+{}
+
+void BTree::max()
+{}
+
+void BTree::displayInOrder()
+{}
+
+void BTree::displayPreOrder()
+{}
+
+void BTree::displayPostOrder()
+{}
+
+
 int main()
 {
 	return 0;
