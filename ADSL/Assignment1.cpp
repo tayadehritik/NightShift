dedc2429e7a0f811d969032ef99f74dad95da594
tayadehritik <braceritchie@protonmail.com>
2020-03-07 10:39:37 +0530

    march long challenge started

--- ADSL/Assignment1.cpp
+++ ADSL/Assignment1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class node
 {
-	private: 
+	public: 
 		node *left, *right;
 		int data;
 };
@@ -14,17 +14,71 @@ class BTree
 	private: 
 		node *root;
 	public:
+		BTree()
+		{
+			root = NULL;
+		}
 		void create();
 		void min();
 		void max();
 		void displayInOrder();
 		void displayPreOrder();
 		void displayPostOrder();
-
+		void recInOrder(node *);
+		node * insert();
 };
 
 void BTree::create()
-{}
+{
+
+	node *temp = new node;
+
+	cout<<"Enter new node data , -1 for NULL";
+	cin>>temp->data;
+	temp->left = NULL;
+	temp->right = NULL;
+	
+	if(root == NULL)
+	{		
+		root = temp;
+		create();
+	}
+	else
+	{
+		node *trav = root;
+		
+		cout<<endl<<trav->data;
+		trav->left = insert();
+		cout<<endl<<trav->data;
+		
+		trav->right = insert();
+
+	}
+
+
+}
+
+node * BTree::insert()
+{
+	node *temp = new node;
+	cout<<endl<<"Enter new node data -1 for NULL";
+	cin>>temp->data;
+	temp->left = NULL;
+	temp->right = NULL;
+	if(temp->data == -1)
+	{
+		return NULL;
+	}
+	else
+	{
+
+		temp->left = insert();
+		temp->right = insert();
+		return temp;
+
+	}
+
+}
 
 void BTree::min()
 {}
@@ -41,8 +95,19 @@ void BTree::displayPreOrder()
 void BTree::displayPostOrder()
 {}
 
+void BTree::recInOrder(node *root)
+{
+	if(root != NULL)
+	{
+		recInOrder(root->left);
+		cout<<root->data<<endl;
+		recInOrder(root->right);
+	}
+}
 
 int main()
 {
+	BTree b;
+	b.create();
 	return 0;
 }
---
+++ chpintu.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+
+class chpintu
+{
+	public static void main(String args[])
+	{
+		System.out.println("here");
+	
+	}
+
+}
