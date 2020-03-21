edf41edb6135678e49fab0b1f6f1562207144b4a
tayadehritik <braceritchie@protonmail.com>
2020-03-21 11:17:07 +0530

    TLE as expected but 2 test cases passed, meaning logic behind the solution is correct

--- engxor.java
+++ engxor.java
@@ -7,7 +7,7 @@ class engxor
 	{
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		for(int k=0;k<t;k++)
+		for(int l=0;l<t;l++)
 		{
 			int n = sc.nextInt();
 			int q = sc.nextInt();
@@ -20,6 +20,39 @@ class engxor
 			for(int i=0;i<q;i++)
 			{
 				queries[i] = sc.nextInt();
+			}
+			//slow solution
+			for(int i=0;i<q;i++)
+			{
+				String[] seqb = new String[n];
+				for(int j=0;j<n;j++)
+				{
+					int temp = seqp[j] ^ queries[i];
+					seqb[j] = Integer.toBinaryString(temp);
+				}
+				int even = 0;
+				int odd = 0;
+				for(int j=0;j<n;j++)
+				{
+					
+					int numberof1 = 0;
+					for(int k=0;k<seqb[j].length();k++)
+					{
+						if(seqb[j].charAt(k) == '1')
+						{
+							numberof1 = numberof1 + 1;
+						}
+					}
+					if(numberof1 % 2 == 0)
+					{
+						even = even + 1;
+					}
+					else
+					{
+						odd = odd + 1;
+					}
+				}
+				System.out.println(even+" "+odd);
 			}
 
 		}
