a05f6c5d60fc81514e6d59204e95c20a180a18a0
tayadehritik <braceritchie@protonmail.com>
2020-03-21 10:55:05 +0530

    input done for engxor.java

--- engxor.java
+++ engxor.java
@@ -9,7 +9,19 @@ class engxor
 		int t = sc.nextInt();
 		for(int k=0;k<t;k++)
 		{
-		
+			int n = sc.nextInt();
+			int q = sc.nextInt();
+			int[] seqp = new int[n];
+			int[] queries = new int[q];
+			for(int i=0;i<n;i++)
+			{
+				seqp[i] = sc.nextInt();
+			}	
+			for(int i=0;i<q;i++)
+			{
+				queries[i] = sc.nextInt();
+			}
+
 		}
 	
 	}
