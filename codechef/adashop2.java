11c3b30a57bc73d1badd13bbcc3f8687171c4d22
tayadehritik <braceritchie@protonmail.com>
2020-03-23 18:50:27 +0530

    adashop2

---
+++ codechef/adashop2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+
+class adashop2
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int k=0;k<t;k++)
+		{
+			int r0 = sc.nextInt();
+			int c0 = sc.nextInt();
+		}
+	}
+
+}
