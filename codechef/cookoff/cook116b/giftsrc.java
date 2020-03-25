a66b9f26566c4573b614b1b7dd390496bb2edb17
tayadehritik <braceritchie@protonmail.com>
2020-03-25 20:43:05 +0530

    input done for giftsrc.java

---
+++ codechef/cookoff/cook116b/giftsrc.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class giftsrc
+{
+
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(sc.readLine());
+        while(t!=0)
+        {
+            int n = Integer.parseInt(sc.readLine());
+            String s = sc.readLine();
+
+            t = t-1;
+        }
+
+
+    }
+
+}
