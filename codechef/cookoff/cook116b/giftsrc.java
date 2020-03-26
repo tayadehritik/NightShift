358c61f3bb9a62768d93b5870bd299ed5d61a5c4
tayadehritik <braceritchie@protonmail.com>
2020-03-26 21:31:16 +0530

    working on giftsrc.java

--- codechef/cookoff/cook116b/giftsrc.java
+++ codechef/cookoff/cook116b/giftsrc.java
@@ -11,8 +11,30 @@ class giftsrc
         int t = Integer.parseInt(sc.readLine());
         while(t!=0)
         {
+            int x = 0;
+            int y = 0;
             int n = Integer.parseInt(sc.readLine());
             String s = sc.readLine();
+            char prev='k';
+            for(int i=0;i<s.length();i++)
+            {
+                char curr = s.charAt(i);
+                if(prev == curr)
+                {
+                    continue;
+                }
+                else
+                {
+                    if(curr == 'U')
+                    {}
+                    else if(curr == 'L')
+                    {}
+                    else if(curr == 'R')
+                    {}
+                    else if(curr == 'D')
+                    {}
+                }
+            }
 
             t = t-1;
         }
