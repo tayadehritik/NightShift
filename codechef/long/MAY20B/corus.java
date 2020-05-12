f48ea05f56bbcd585f690b904d1cbb60d020ffca
tayadehritik <braceritchie@protonmail.com>
2020-05-12 16:21:35 +0530

    solved corus with partially correct answer, only one test case failed that too with only TLE not WA

--- codechef/long/MAY20B/corus.java
+++ codechef/long/MAY20B/corus.java
@@ -6,7 +6,64 @@ class corus
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int k=0;k<t;k++)
+		{
+			int n = sc.nextInt();
+			int q = sc.nextInt();
+			sc.nextLine();
+			String s = sc.nextLine();
+			int IsolationCenters[] = new int[q];
+			for(int i=0;i<q;i++)
+			{
+				IsolationCenters[i] = sc.nextInt();
+				int num = pendingQueueNumber(IsolationCenters[i],s,n);
+				System.out.println(num);
+			}
+		}
 	
+	}
+
+	static int pendingQueueNumber(int IsolationCenters,String Cases, int NumberOfCases)
+	{
+		Dictionary CasesAndNumbers = getAllTypesOfCasesAndTheirNumbers(Cases,NumberOfCases);
+		
+		int pendingCases = 0;		
+	
+		for(Enumeration i = CasesAndNumbers.elements(); i.hasMoreElements();) 
+        	{ 
+           		
+			int temp = (int) i.nextElement();
+			if(temp > IsolationCenters)
+			{
+				pendingCases = pendingCases + (temp - IsolationCenters);
+			}	
+        	} 
+
+		return pendingCases;
+	}
+
+	static Dictionary getAllTypesOfCasesAndTheirNumbers(String Cases,int NumberOfCases)
+	{
+		Dictionary CasesAndNumbers = new Hashtable();
+
+		for(int i=0;i<NumberOfCases;i++)
+		{
+		
+			/*CasesAndNumbers.put(Cases.charAt(i),1);*/
+			if(CasesAndNumbers.get(Cases.charAt(i)) == null)
+			{
+				CasesAndNumbers.put(Cases.charAt(i),1);
+			}
+			else
+			{
+				int temp =(int) CasesAndNumbers.get(Cases.charAt(i));
+				CasesAndNumbers.put(Cases.charAt(i),temp+1);
+			}
+		}
+
+
+		return CasesAndNumbers;
 	}
 
 }
