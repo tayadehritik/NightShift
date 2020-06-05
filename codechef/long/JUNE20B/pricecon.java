5b49145dc7fbc484a2e0687435651e842a7ab72e
tayadehritik <braceritchie@protonmail.com>
2020-06-05 19:29:51 +0530

    pricecon - correct answer

--- codechef/long/JUNE20B/pricecon.java
+++ codechef/long/JUNE20B/pricecon.java
@@ -5,6 +5,32 @@ class pricecon
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int l=0;l<t;l++)
+		{
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			int arr[] = new int[n];
+			int InitialSum = 0;
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+				InitialSum = InitialSum + arr[i];
+			}
+			int sum = 0;
+			for(int i=0;i<n;i++)
+			{
+				if(arr[i] > k)
+				{
+					sum = sum + k;
+				}
+				else
+				{
+					sum = sum + arr[i];
+				}
+			}
 
+			System.out.println(InitialSum - sum);
+		}
 	}
 }
