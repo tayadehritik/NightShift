270ff0c9796b80794012662448c623f7b7771d2a
tayadehritik <braceritchie@protonmail.com>
2020-06-11 19:03:39 +0530

    fully correct answer for chficrm , simple mistake of not adding coins in register already with value 0

--- codechef/long/JUNE20B/chficrm.java
+++ codechef/long/JUNE20B/chficrm.java
@@ -27,49 +27,38 @@ class chficrm
 	{
 		int currentDeposit = 0;
 		HashMap<Integer, Integer> register = new HashMap<>();
-		boolean flag = true;	
+		boolean flag = true;
+		register.put(5,0);
+		register.put(10,0);
+		register.put(15,0);	
 		for(int i=0;i<n;i++)
 		{
 		
-			if(register.containsKey(arr[i]))
+			register = putInRegister(register, arr[i]);
+			//System.out.println(register);
+			Integer change = calculateChange(arr[i]);
+			if(change != 0)
 			{
-				Integer val = register.get(arr[i]);
-				register.put(arr[i],val+1);
-			}
-			else
-			{
-				register.put(arr[i],1);
-			}
+				boolean changeStatus = checkIfThereIsChangeInRegister(register,change);
 
-
-			int returnMoney = arr[i] - 5;
-
-			if(returnMoney != 0)
-			{
-				if(register.containsKey(returnMoney) == false)
+				if(changeStatus == true)
 				{
-					flag = false;
-					break;
+					register = removeFromRegister(register,change);	
 				}
 				else
 				{
-					if(register.get(returnMoney) == 0)
-					{
-						flag = false;
-						break;
-					}
-					else
-					{
-						Integer newVal = register.get(returnMoney);
-						register.put(returnMoney, newVal-1);
-					}
-				
+					register = removeFromRegister(register,arr[i]);
+					flag = false;
+					break;
 				}
 			}
+					
 
+			//System.out.println(returnMoney);
 		
 		}
-				
+		//System.out.println(register);
+
 		if(flag == true)
 		{
 			return "YES";
@@ -80,4 +69,78 @@ class chficrm
 		}
 
 	}
+
+	public static HashMap<Integer,Integer> putInRegister(HashMap<Integer,Integer> register, int cash)
+	{
+		
+		if(register.containsKey(cash))
+		{
+			register.put(cash,register.get(cash)+1);	
+		}
+		else
+		{
+			register.put(cash,1);
+		}	
+	
+		return register;
+	}
+	public static boolean checkIfThereIsChangeInRegister(HashMap<Integer,Integer> register, int change)
+	{
+			
+		
+		if(register.containsKey(change))
+		{
+						
+			if(register.get(change) >= 1)
+			{
+				return true;
+			}
+			else
+			{
+				if(change == 10)
+				{
+					
+					if(register.get(5) >= 2)
+					{
+						return true;
+					}
+					else
+					{
+					
+						return false;
+					}
+				
+				}
+				else
+				{
+					return false;
+				}
+			
+			}
+		}
+		else
+		{
+			return false;
+		}
+		
+		
+	}
+
+	public static HashMap<Integer,Integer> removeFromRegister(HashMap<Integer,Integer> register, int cashToRemove)
+	{
+		if(register.get(cashToRemove)<1)
+		{
+			register.put(5,register.get(5)-2);
+		}
+		else
+		{
+			register.put(cashToRemove, register.get(cashToRemove)-1);
+		}
+		return register;
+	}
+	public static int calculateChange(int cash)
+	{
+		return cash-5;	
+	}
+
 }
